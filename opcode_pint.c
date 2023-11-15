71daa3044e4d04e33c1aa1c777db4ee49f60cce3
PeinNotFound <akatsuki_ahmed@yahoo.com>
2023-11-15 05:52:52 -0800

    opcode

---
+++ opcode_pint.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+
+/**
+ * pint - Prints the value at the top of the stack.
+ * @stack: A pointer to the top of the stack.
+ * @line_number: The current line number in monty bytecode file.
+ */
+
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (!stack || !*stack)
+	{
+		pint_error(line_number);
+	}
+
+	printf("%d\n", (*stack)->n);
+}
---
+++ opcode_pop.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+/**
+ * pop - Removes the top element of the stack.
+ * @stack: A pointer to the top of the stack.
+ * @line_number: The current line number in monty bytecode file.
+ */
+
+void pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (!stack || !*stack)
+	{
+		pop_error(line_number);
+	}
+
+	temp = *stack;
+	*stack = (*stack)->next;
+	if (*stack)
+	{
+		(*stack)->prev = NULL;
+	}
+	free(temp);
+}
