c02e7b8ada20ce179d344eba52afc64bc49459b1
PeinNotFound <akatsuki_ahmed@yahoo.com>
2023-11-15 06:00:14 -0800

    exe2

--- execute.c
+++ execute.c
@@ -1,4 +1,5 @@
 #include "monty.h"
+
 /**
  * execute_f - Execute an opcode-based function.
  *
@@ -7,45 +8,79 @@
  * @numlin: The current line number.
  * @file: A pointer to the file.
  *
- * This function executes a function based on the provided opcode and handles
- * known instructions. It returns 1 for valid instructions or 0 for unknown
- * ones,
- * printing an error message and exiting in the latter case.
- *
- * Return: 1 for a valid instruction, 0 for unknown instructions.
+ * This function executes a function based on the provided opcode
+ * Return: int 1 if the command executed esle 0;
  */
 
 int execute_f(char *content, stack_t **stack, unsigned int numlin, FILE *file)
 {
-	instruction_t Spe_func[] = {
-		{"push", push},
-		{"pall", pall},
-		{"pint", pint},
-		{"pop", pop},
-		{"swap", swap},
-		{"add", add},
-		{"nop", nop},
-		{NULL, NULL}};
-	int i = 0;
+	char *content_copy = strdup(content), *token, *value;
+	int valu, i = 0;
+	instruction_t Spe_func[] = {{"push", push}, {"pall", pall}, {"pint", pint},
+	{"pop", pop}, {"swap", swap}, {"add", add}, {"nop", nop}, {"sub", sub},
+	{"div", div_}, {"mul", mul}, {"mod", mod}, {"pchar", pchar}, {"pstr", pstr},
+	{"div", div_}, {"rotl", rotl}, {"rotr", rotr}, {NULL, NULL}};
 
-	if (content == NULL || content[0] == '#')
+	token = strtok(content_copy, " \n\t");
+	value = strtok(NULL, " \n\t");
+	if (token == NULL || token[0] == '#')
 	{
-		/* Skip comments and empty lines*/
+		free(content_copy);
 		return (0);
 	}
-
+	if (strcmp(token, "push") == 0)
+	{
+		if (value == NULL || !is_valid_integer(value))
+		{
+			free(content_copy);
+			push_error(numlin, file);
+			return (0);
+		}
+		valu = atoi(value);
+	}
+	else
+		valu = numlin;
 	while (Spe_func[i].opcode)
 	{
-		if (strcmp(Spe_func[i].opcode, content) == 0)
+		if (strcmp(Spe_func[i].opcode, token) == 0)
 		{
-			Spe_func[i].f(stack, numlin);
+			free(content_copy);
+			pchar__error(stack, numlin, file, content);
+			Spe_func[i].f(&*stack, valu);
 			return (1);
 		}
 		i++;
 	}
-
-	fprintf(stderr, "L%d: unknown instruction %s\n", numlin, content);
-	fclose(file);
-	exit(EXIT_FAILURE);
+	free(content);
+	global_error(numlin, file, content_copy, stack);
 	return (0);
+}
+
+/**
+ * pchar__error - Handles errors when attempting to print a character
+ * from an empty stack.
+ *
+ * @stack: A pointer to the stack.
+ * @nubr: The current line number.
+ * @file: A pointer to the file
+ * @tokn: The token (instruction) being processed.
+ *
+ * This function checks if the stack is empty and the instruction
+ * is "pchar." If both
+ * conditions are met, it prints an error message to the standard
+ * error stream and
+ * exits with a failure status.
+ *
+ * Return: No return value; exits with failure status.
+ */
+
+void pchar__error(stack_t **stack, unsigned int nubr, FILE *file, char *tokn)
+{
+	if ((!stack || !*stack) && strcmp(tokn, "pchar") == 0)
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", nubr);
+		free(tokn);
+		fclose(file);
+		exit(EXIT_FAILURE);
+	}
 }
